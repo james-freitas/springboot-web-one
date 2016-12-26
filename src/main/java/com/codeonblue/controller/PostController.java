738658b23dd9f09598752979c8024a7b621dcb15
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2016-12-26 16:40:23 -0200

    Internationalization

---
+++ src/main/java/com/codeonblue/controller/PostController.java
@@ -0,0 +1,14 @@
+package com.codeonblue.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PostController {
+
+    @RequestMapping("/posts")
+    public String list(){
+        return "blog/posts";
+    }
+
+}
