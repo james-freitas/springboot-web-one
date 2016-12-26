f0befcca62621b02f5056638dc061dc62a2903be
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2016-12-26 13:51:29 -0200

    First commit

---
+++ src/main/java/com/codeonblue/SpringBootWebAppOneApplication.java
@@ -0,0 +1,12 @@
+package com.codeonblue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppOneApplication.class, args);
+	}
+}
